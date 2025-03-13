cbe8bf4d06596b57884578e2e85ecc726cba586f
sumit <sumitronaldo111@gmail.com>
2025-03-14 04:45:57 +0530

    dd

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import os
 import mysql.connector
 import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'Password'
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Baripada@123")
-DB_NAME = os.getenv("DB_NAME", "user_database")
+app.secret_key = os.getenv('SECRET')
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 
 db = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
 cursor = db.cursor()
