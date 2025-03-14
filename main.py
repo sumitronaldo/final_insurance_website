f9226a93ca762cda7366b43daa0dcd1b2dc4fff0
sumit <sumitronaldo111@gmail.com>
2025-03-14 15:19:29 +0530

    asd

--- main.py
+++ main.py
@@ -4,21 +4,53 @@ import mysql.connector
 import pymysql
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 app = Flask(__name__)
 
+
 app.secret_key = os.getenv('SECRET')
 
-DB_HOST = os.getenv('DB_HOST')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 db = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
 cursor = db.cursor()
-    
+   
+def init_db():
+    try:
+        conn = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            cursorclass=pymysql.cursors.DictCursor
+        ) 
+        cursor = conn.cursor()
+        
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
+        conn.select_db(DB_NAME)
+        
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            dob DATE NOT NULL,
+            username VARCHAR(50) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL
+            )
+        """)
+        
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Database and table initialized successfully.")
+    except pymysql.MySQLError as err:
+        print(f"Error initializing database: {err}")
 
+init_db()
 #def create_user(username, password):
 #    conn = get_db_conn()
 #    cursor = conn.cursor()
@@ -87,6 +119,7 @@ def dashboard():
 @app.route('/logout')
 def logout():
     session.pop('user', None)
+    flash("Logged out successfully!!","info")
     return redirect(url_for('login'))
 
 @app.route('/about')
